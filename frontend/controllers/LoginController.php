d819250f661b0a11f7457ac257af157fcbe24cb5
kopa <1113288996@qq.com>
2018-08-06 13:13:11 +0800

    初始化版本

---
+++ frontend/controllers/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+namespace frontend\controllers;
+
+use Yii;
+use yii\base\InvalidParamException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use common\models\LoginForm;
+use frontend\models\PasswordResetRequestForm;
+use frontend\models\ResetPasswordForm;
+use frontend\models\SignupForm;
+use frontend\models\ContactForm;
+
+/**
+ * Login Web
+ */
+class LoginController extends Controller
+{
+   function actionminiLogin()
+   {
+       /*
+        * 小程序微信登录接口
+        * */
+
+   }
+}
---
+++ frontend/messages/zh-CN/common.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kopa
+ * Date: 2018/8/3
+ * Time: 下午4:15
+ */
+
+return [
+  'Blog'=>'博客',
+];
